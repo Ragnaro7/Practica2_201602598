bb89fc2f70a18ca9b39f2c4b8647d2668eef554d
Hector <Hector@192.168.0.13>
2017-10-14 22:46:32 -0600

    >

---
+++ src/practica2_201602598/Practica2_201602598.java
@@ -0,0 +1,15 @@
+
+package practica2_201602598;
+
+public class Practica2_201602598 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculadora cal=new Calculadora();
+        cal.setVisible(true);
+        
+        
+        
+    }
+    
+}
